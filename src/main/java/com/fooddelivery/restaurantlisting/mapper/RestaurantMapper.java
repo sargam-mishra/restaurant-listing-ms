6ef7f4470154267a8458e15e5a788d8713ba23e1
sargam-mishra <51758084+sargam-mishra@users.noreply.github.com>
2025-02-27 16:50:40 +0530

    restaurant listing ms

---
+++ src/main/java/com/fooddelivery/restaurantlisting/mapper/RestaurantMapper.java
@@ -0,0 +1,18 @@
+package com.fooddelivery.restaurantlisting.mapper;
+
+
+import com.fooddelivery.restaurantlisting.dto.RestaurantDTO;
+import com.fooddelivery.restaurantlisting.entity.Restaurant;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface RestaurantMapper {
+
+    RestaurantMapper INSTANCE = Mappers.getMapper(RestaurantMapper.class);
+
+    Restaurant mapRestaurantDTOToRestaurant(RestaurantDTO restaurantDTO);
+
+    RestaurantDTO mapRestaurantToRestaurantDTO(Restaurant restaurant);
+
+}
